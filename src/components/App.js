467a46c94ef34f8b053063244dad524c4e467773
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-08-01 17:05:53 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Header } from "./App.styled";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Header>Privet</Header>
+      </div>
+    </div>
+  );
+};
+
+export default App;
